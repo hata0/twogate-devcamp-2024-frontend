91a7cacbdee465a4228808c0767972a6852a99c4
hata0 <haataa0100@gmail.com>
2024-08-22 23:12:09 +0900

    feat: httpHandlerFactory を作成

---
+++ src/lib/msw/httpHandlerFactory.ts
@@ -0,0 +1,39 @@
+import { http, HttpResponse, HttpResponseResolver } from "msw";
+
+type HandlerFactoryArgs = {
+  isNetworkError?: boolean;
+  error?: {
+    message: string;
+    status: number;
+  };
+  resolver?: HttpResponseResolver;
+};
+
+export const httpHandlerFactory = (
+  method: keyof typeof http,
+  path: string,
+  defaultResolver: HttpResponseResolver,
+) => {
+  return ({ error, isNetworkError, resolver }: HandlerFactoryArgs = {}) => {
+    if (isNetworkError) {
+      return http[method](path, () => {
+        return HttpResponse.error();
+      });
+    } else if (error) {
+      return http[method](path, () => {
+        return HttpResponse.json(
+          {
+            error: error.message,
+          },
+          {
+            status: error.status,
+          },
+        );
+      });
+    } else if (resolver) {
+      return http[method](path, resolver);
+    } else {
+      return http[method](path, defaultResolver);
+    }
+  };
+};
