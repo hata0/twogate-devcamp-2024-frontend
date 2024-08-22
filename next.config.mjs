da8c30634f000d3671576f2769301404104b702e
hata0 <haataa0100@gmail.com>
2024-08-22 20:58:13 +0900

    feat: ファイルを整理してフォーマットを実行

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    tsconfigPath: process.env.NODE_ENV === "test" 
-      ? "tsconfig.test.json"
-      : "tsconfig.json",
-  }
+    tsconfigPath: process.env.NODE_ENV === "test" ? "tsconfig.test.json" : "tsconfig.json",
+  },
 };
 
 export default nextConfig;
