df0f8e241ff0c8c9f71d18003d77d88fcbb70930
hata0 <haataa0100@gmail.com>
2024-08-23 11:05:52 +0900

    feat: cors 対策を追加

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,28 @@ const nextConfig = {
   typescript: {
     tsconfigPath: process.env.NODE_ENV === "test" ? "tsconfig.test.json" : "tsconfig.json",
   },
+  headers: async () => {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: process.env.NEXT_PUBLIC_BACKEND_URL ?? "",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization, ngrok-skip-browser-warning",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
