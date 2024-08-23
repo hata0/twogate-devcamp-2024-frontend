921f03858cd1fcde4fff2e1b1615a6db52c35a91
hata0 <haataa0100@gmail.com>
2024-08-23 12:04:27 +0900

    feat: liff の確認用に実装

--- src/features/top/components/Top/index.tsx
+++ src/features/top/components/Top/index.tsx
@@ -1,8 +1,28 @@
+"use client";
+
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 import { Button } from "@/components/shadcn/ui/button";
+import { BACKEND_URL } from "@/constants/backend-url";
+import { useLiffContext } from "@/providers/LiffProvider";
+import { fetcherWithAuth } from "@/utils/fetcherWithAuth";
 
 export const Top = () => {
+  const { liff } = useLiffContext();
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    void (async () => {
+      const idToken = liff?.getIDToken() ?? "";
+      const { res } = await fetcherWithAuth(`${BACKEND_URL}`, idToken);
+      if (res?.ok) {
+        const data = (await res.json()) as { message: string };
+        setMessage(data.message);
+      }
+    })();
+  });
+
   return (
     <div>
       <div className="flex w-full items-center justify-center space-x-2">
@@ -12,6 +32,7 @@ export const Top = () => {
         <Button asChild>
           <Link href="/recruitments">運動の募集一覧</Link>
         </Button>
+        <div>message: {message}</div>
       </div>
     </div>
   );
