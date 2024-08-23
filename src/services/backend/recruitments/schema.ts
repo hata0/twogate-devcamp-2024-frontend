15fb98463b7fa97d269073cb0f47b04ce1f7db39
hata0 <haataa0100@gmail.com>
2024-08-23 19:56:29 +0900

    feat: 文字列から enum に変換できるように修正

--- src/services/backend/recruitments/schema.ts
+++ src/services/backend/recruitments/schema.ts
@@ -3,6 +3,14 @@ import { z } from "zod";
 import { RecruitmentType } from "./type";
 
 export const createRecruitmentInputSchema = z.object({
-  recruitmentType: z.nativeEnum(RecruitmentType),
+  recruitmentType: z.preprocess((val) => {
+    if (typeof val === "string") {
+      const parsed = parseInt(val, 10);
+      if (!isNaN(parsed)) {
+        return parsed;
+      }
+    }
+    return val;
+  }, z.nativeEnum(RecruitmentType)),
   title: z.string().min(1, "募集内容を入力してください"),
 });
