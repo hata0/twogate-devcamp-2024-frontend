10edf5ce2acaca19f73b4eb57c2001c6413b8662
hata0 <haataa0100@gmail.com>
2024-08-23 13:45:40 +0900

    feat: 募集一覧ページにリンクを追加

--- src/features/recruitments/components/Recruitments/index.tsx
+++ src/features/recruitments/components/Recruitments/index.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
+import { Button } from "@/components/shadcn/ui/button";
+
 export const Recruitments = () => {
   return (
     <div>
-      <div>hello</div>
+      <Button asChild>
+        <Link href="/recruitments/new">新しく募集する</Link>
+      </Button>
     </div>
   );
 };
