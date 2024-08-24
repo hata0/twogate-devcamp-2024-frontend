e3d17ddc9f748bfd496bc032ae633102646a7f38
hata0 <haataa0100@gmail.com>
2024-08-24 19:15:40 +0900

    fix: useSearchParams が正しく使えるよう修正

--- src/app/recruitments/page.tsx
+++ src/app/recruitments/page.tsx
@@ -1,5 +1,11 @@
+import { Suspense } from "react";
+
 import { Recruitments } from "@/features/recruitments/components/Recruitments";
 
 export default function RecruitmentsPage() {
-  return <Recruitments />;
+  return (
+    <Suspense>
+      <Recruitments />
+    </Suspense>
+  );
 }
