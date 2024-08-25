320667385bd9cc0d346404bb898948ab885ca2c6
hata0 <haataa0100@gmail.com>
2024-08-25 09:37:22 +0900

    feat: getFriends のパスを修正

--- src/services/backend/friends/index.ts
+++ src/services/backend/friends/index.ts
@@ -4,7 +4,7 @@ import { fetcherWithAuth } from "@/utils/fetcherWithAuth";
 export * from "./type";
 
 // TODO: 厳密には友達をページ単位で取得させる必要がある
-export const createPath = () => `${BACKEND_URL}/friends`;
+export const createPath = () => `${BACKEND_URL}/users/me/friends`;
 
 export const getFriends = (idToken: string) =>
   fetcherWithAuth(createPath(), idToken, {
